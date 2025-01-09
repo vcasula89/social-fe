26991fdecc1dd707aa91a758c28e1c36b7872d54
Matteo Atzeni <atzee.matte@gmail.com>
2025-01-09 16:14:40 +0100

    modifica

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import CreatePostComponent from '../CreatePostComponent/CreatePostComponent.jsx'
 import PostListComponent from '../PostListComponent/PostListComponent.jsx'
+import React from 'react'
 
 const HomePage = () =>{
     return <>
-    <CreatePostComponent/>
+    
     <PostListComponent/>
-
+    <CreatePostComponent/>
     </>
 }
 export default HomePage;
